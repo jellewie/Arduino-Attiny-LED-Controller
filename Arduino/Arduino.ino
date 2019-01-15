cd01e3a55cba4a264bda33760c9a04942eacf163
JelleWho <1545223+jellewie@users.noreply.github.com>
2019-01-15 21:30:08 +0100

    Tested and fixed some things
    Seem to work as intended

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,31 +1,35 @@
 /*
-  Program written by JelleWho for a standaone chip that takes 5 digtal inputs and convert them to a FastLed controlled led strip
+  Program written by JelleWho for a standalone chip that takes 5 digital inputs and convert them to a FastLed controlled led strip
 */
-//#define FASTLED_INTERNAL
-#include <FastLED.h>                                        //Include the FastLED library to control the LEDs in a simple fashion
+
+//Just some configurable things
+const static int TotalLEDs = 100;                                    //The total amounts of LEDs in the strip
+const static byte DelayAnimationBlink = 50;                         //Delay in ms for an update in the animation (technically the minimum time)
+const static byte DelayAnimationFlash = 250;                        //^^
+const static byte DelayAnimationMove = 20;                          //^^
+const static byte Brightness = 255;                                 //The brightness of the LEDs (higher is brightener)
+const static byte MoveAmount = 2;                                   //Quantity of the sections in the move animation
+const static byte MoveLength = 5;                                   //Length of the sections in the move animation
+
+
 //static const (read only) █ <variable type> https://www.arduino.cc/reference/en/#variables █ <Pin ║ Digital, Analog, pWn ║ Input, Output║ _name>
 static const byte PAO_LED = 0;                                      //Where the <LED strip> is connected to
-static const byte PDI_Red = 1;                                      //turn RED LEDs on
-static const byte PDI_Green = 2;                                    //turn Green LEDs on
-static const byte PDI_Blue = 3;                                     //turn Blue LEDs on
+static const byte PDI_Red = 1;                                      //Turn RED LEDs on
+static const byte PDI_Green = 2;                                    //Turn Green LEDs on
+static const byte PDI_Blue = 3;                                     //Turn Blue LEDs on
 static const byte PDI_Blink = 4;                                    //animation = Blinking like a turn signal
 static const byte PDI_Flash = 0;                                    //animation = Just flash all together (else (&!^) just steady color)  (THIS IS THE ANALOG PIN NUMBER)
-//Just some configurable things
-static const int TotalLEDs = 40;
-const static byte DelayAnimationBlink = 50;                         //Delay in ms for an update in the animation (minimum time)
-const static byte DelayAnimationFlash = 250;                        //^^
-const static byte DelayAnimationMove = 30;                          //^^
-//Just some numbers we need to transfer around..
+#include <FastLED.h>                                                //Include the FastLED library to control the LEDs in a simple fashion
 CRGB LEDs[TotalLEDs];                                               //This is an array of LEDs. One item for each LED in your strip.
 void setup() {                                                      //This code runs once on start-up
   pinMode(PAO_LED,    OUTPUT);                                      //Sometimes the Arduino needs to know what pins are OUTPUT and what are INPUT,
-  pinMode(PDI_Red,    INPUT);                                       // since it could get confused and create an error. So it's set manual here
+  pinMode(PDI_Red,    INPUT);                                       //Since it could get confused and create an error. So it's set manual here
   pinMode(PDI_Green,  INPUT);                                       //^^
   pinMode(PDI_Blue,   INPUT);                                       //^^
   pinMode(PDI_Blink,  INPUT);                                       //^^
   pinMode(PDI_Flash,  INPUT);                                       //^^
   FastLED.addLeds<WS2812B, PAO_LED, GRB>(LEDs, TotalLEDs);          //Set the LED type and such
-  FastLED.setBrightness(10);                                        //Scale brightness
+  FastLED.setBrightness(Brightness);                                //Scale brightness
   fill_solid(&(LEDs[0]), TotalLEDs, CRGB(0, 0, 0));                 //Set the whole LED strip to be off (Black)
   FastLED.show();                                                   //Update
 }
@@ -47,16 +51,14 @@ void loop() {                                                       //Keep loopi
     Green = 255;                                                    //^^
   if (digitalRead(PDI_Blue) == HIGH)                                //^^^^
     Blue = 255;                                                     //^^^^
-  if (digitalRead(PDI_Blink) == HIGH) {                             //If the LEDs need to show an animation    
-    if (analogRead(PDI_Flash) >= 900) {
+  if (digitalRead(PDI_Blink) == HIGH) {                             //If the LEDs need to show an animation
+    if (analogRead(PDI_Flash) >= 900) {                             //If the LEDs need to show both animations, (so show the third animation)
       if (Mode != 3) {                                              //If we just moved into this Mode (and are not yet in the correct state)
         Mode = 3;                                                   //Set this Mode
         fill_solid(&(LEDs[0]), TotalLEDs, CRGB(0, 0, 0));           //Set the whole LED strip to be off (Black)
         UpdateLEDs = true;                                          //Update
       }
       static int CounterMove = 0;                                   //Create a counter (To store the position in)
-      static byte MoveAmount = 2;                                   //Quantity of the sections
-      static byte LengthMove = 5;                                   //Length of the sections
       int OffsetMove = TotalLEDs / MoveAmount;                      //Calculation for calculating offset from first Position
       int poss[MoveAmount];                                         //Array for saving the positions of the sections
       if (CounterMove >= TotalLEDs) {                               //Will check if the main position is bigger than the total
@@ -75,11 +77,11 @@ void loop() {                                                       //Keep loopi
           posX = poss[i] - TotalLEDs;                               //Subtract the total amount of LEDs from the position number
         } else                                                      //Otherwise it will just use the position data without modifying it
           posX = poss[i];                                           //Just use the position number
-        if (posX <= (TotalLEDs - LengthMove)) {                     //If the whole section ends before the total amount is reached it wil just us the normal way of setting the LEDs
-          fill_solid(&(LEDs[posX]), LengthMove, CRGB(Red, Green, Blue)); //With the standard fill solid command from FastLED, LEDs[posX] PosX stands for beginning position, LengthMove will stand for the size of the sections and the last one is the colour
-        } else if ((posX >= (TotalLEDs - LengthMove)) && (posX <= TotalLEDs)) {//This will check if the thing is beyond the total amount of LEDs
-          int calc1 = (TotalLEDs - (posX + LengthMove)) * -1;       //Calculates the amount of LEDs which need to be set from the beginning
-          int calc2 = LengthMove - calc1;                           //Calculates the amount of LEDs which need to be set at the last so the total will be reached but wont be bigger than the total
+        if (posX <= (TotalLEDs - MoveLength)) {                     //If the whole section ends before the total amount is reached it will just us the normal way of setting the LEDs
+          fill_solid(&(LEDs[posX]), MoveLength, CRGB(Red, Green, Blue)); //With the standard fill solid command from FastLED, LEDs[posX] PosX stands for beginning position, MoveLength will stand for the size of the sections and the last one is the color
+        } else if ((posX >= (TotalLEDs - MoveLength)) && (posX <= TotalLEDs)) {//This will check if the thing is beyond the total amount of LEDs
+          int calc1 = (TotalLEDs - (posX + MoveLength)) * -1;       //Calculates the amount of LEDs which need to be set from the beginning
+          int calc2 = MoveLength - calc1;                           //Calculates the amount of LEDs which need to be set at the last so the total will be reached but won’t be bigger than the total
           fill_solid(&(LEDs[posX]), calc2, CRGB(Red, Green, Blue)); //Fills the LEDs at the end of the strip
           fill_solid(&(LEDs[0])   , calc1, CRGB(Red, Green, Blue)); //Fills the LEDs at the beginning of the strip
         }
@@ -94,7 +96,7 @@ void loop() {                                                       //Keep loopi
         UpdateLEDs = true;                                          //Update
         static byte CounterBlink = 0;                               //Create a counter (storing the amounts of LEDs on)
         static byte LengthBlink = TotalLEDs;                        //Total length of LEDs to blink
-        static byte AlwaysOn = 1;                                   //Howmuch LEDs should always be on
+        static byte AlwaysOn = 1;                                   //How much LEDs should always be on
         fill_solid(&(LEDs[0]), LengthBlink,  CRGB(0, 0, 0));        //Turn LEDs off
         fill_solid(&(LEDs[0]), AlwaysOn,     CRGB(Red, Green, Blue));//Set some LEDs to be always on
         fill_solid(&(LEDs[0]), CounterBlink, CRGB(Red, Green, Blue));//Set the counter amount of LEDs on (this will increase)
